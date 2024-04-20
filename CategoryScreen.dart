f09c116ab49b5668a0f9005ae5bb82a2020200ff
CemBC <119740765+CemBC@users.noreply.github.com>
2024-04-20 22:33:54 +0300

    Creating First Screen
    Later I will implement RecipeListScreen

---
+++ CategoryScreen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class CategoryScreen extends StatelessWidget{
+  const CategoryScreen({super.key});
+
+  Widget build( BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Categories"),),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => RecipeListScreen(category: 'italian')),
+                );
+              },
+              child: Text('Italian'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => RecipeListScreen(category: 'indian')),
+                );
+              },
+              child: Text('Indian'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => RecipeListScreen(category: 'turkish')),
+                );
+              },
+              child: Text('Turkish'),
+            ),
+          ],
+        )
+      )
+    );
+  }
+}
